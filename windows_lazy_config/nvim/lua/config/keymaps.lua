700c1581ab13a13b3f36503879221e4f9a63b768
angkiki <gabrielangzw@gmail.com>
2025-03-23 22:16:49 +0800

    neovim 0.10.x lazy config

---
+++ windows_lazy_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,49 @@
+-- line numbers
+vim.opt.nu = true
+-- relative line numbers
+vim.opt.relativenumber = true
+
+-- indenting to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- ???
+vim.opt.expandtab = true
+
+-- no line wrapping
+vim.opt.wrap = false
+
+-- incremental searching highlighting
+vim.opt.incsearch = true
+
+-- leaves 8 spaces at the bottom of the file
+-- while you are scrolling
+vim.opt.scrolloff = 8
+
+-- ensures that the left column always
+-- has space that doesn't cause LSP hints
+-- to cause the side bar to move
+vim.o.signcolumn = "yes"
+
+-- this remaps the default leader key to <Space>
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- this remaps :Ex to <leader>pv
+-- which allows us to go back to file explorer
+-- by doing <leader>pv
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- <leader>p allows us to paste without losing
+-- whatever is in the "yanked" buffer
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without replacing clipboard" })
+
+
+-- for LSPs
+-- when there's a linting hint/error etc.
+vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+
+
+
