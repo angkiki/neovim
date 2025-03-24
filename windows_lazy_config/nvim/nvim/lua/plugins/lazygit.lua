7ba00fa40d4ba43d186ee3a23db746e5deacce95
angkiki <gabrielangzw@gmail.com>
2025-03-24 15:45:14 +0800

    improve lazy configs

---
+++ windows_lazy_config/nvim/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,30 @@
+-- nvim v0.8.0
+-- this only works if you have lazy git installed on your system
+-- for windows:
+-- LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+-- curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+-- tar xf lazygit.tar.gz lazygit
+-- sudo install lazygit -D -t /usr/local/bin/
+--
+-- for macOS
+-- brew install lazygit
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+}
--- windows_lazy_config/nvim/nvim/lua/plugins/rose-pine.lua
+++ windows_lazy_config/nvim/nvim/lua/plugins/rose-pine.lua
@@ -1,11 +1,12 @@
 -- lua/plugins/rose-pine.lua
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    enabled = false, -- using tokyonight for now
+    config = function()
+        vim.cmd("colorscheme rose-pine")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end
+    end
 }
-
