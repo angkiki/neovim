1a001217323349ee751ef374014b6cbfd19e3cb8
angkiki <gabrielangzw@gmail.com>
2025-04-17 13:00:59 +0800

    add typescript react to conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
                 lua = { "stylua" },
                 javascript = { "prettierd", "prettier", stop_after_first = true },
                 typescript = { "prettierd", "prettier", stop_after_first = true },
+                typescriptreact = { "prettierd", "prettier", stop_after_first = true },
             },
             format_on_save = {
                 timeout_ms = 2000,
