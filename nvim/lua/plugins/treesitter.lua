b675477954dc719ebbb453bddeddf15388ce477e
angkiki <gabrielangzw@gmail.com>
2025-04-28 11:41:39 +0800

    fix treesitter fold

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -24,5 +24,6 @@ return {
         vim.opt.foldmethod = "expr"
         vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
         vim.opt.foldenable = false
+        vim.opt.foldlevel = 99
     end
 }
