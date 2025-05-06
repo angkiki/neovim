f08501a5e47197817425f00e75cf44955177f9e6
angkiki <gabrielangzw@gmail.com>
2025-05-06 11:32:49 +0800

    add barbar, remove nvim-tree bindings

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,27 +2,7 @@ return {
     'kyazdani42/nvim-tree.lua',
     config = function()
         local nt = require("nvim-tree")
-        local api = require("nvim-tree.api")
-
-        local function smart_open(node)
-            if node.type == "directory" or node.nodes ~= nil then
-                -- it's a folder
-                api.node.open.edit(node)
-                return
-            end
-
-            local listed_buffers = vim.fn.getbufinfo({ buflisted = 1 })
-            local real_buffers = vim.tbl_filter(function(b)
-                return b.name ~= "" and not b.name:match("NvimTree_")
-            end, listed_buffers)
-
-            if #real_buffers > 0 then
-                vim.cmd("tabnew")
-                api.node.open.edit(node)
-                vim.cmd("NvimTreeFindFile")
-            end
-            api.node.open.edit(node)
-        end
+        -- local api = require("nvim-tree.api")
 
         nt.setup({
             view = {
@@ -35,21 +15,6 @@ return {
                 dotfiles = false, -- Show dotfiles
                 git_ignored = false,
             },
-            on_attach = function(bufnr)
-                vim.keymap.set("n", "<CR>", function()
-                    local node = api.tree.get_node_under_cursor()
-                    smart_open(node)
-                end, { buffer = bufnr, noremap = true, silent = true })
-
-                -- Custom key mapping for Tab: toggle between tabs
-                vim.keymap.set("n", "<Tab>", "<cmd>tabnext<CR>", { noremap = true, silent = true })
-                vim.keymap.set("n", "<S-Tab>", "<cmd>tabprevious<CR>", { noremap = true, silent = true })
-
-                -- for opening the file in a new split
-                vim.keymap.set("n", "<leader>es", api.node.open.vertical, { noremap = true, silent = true })
-                -- for opening the file in a new tab
-                vim.keymap.set("n", "<leader>et", api.node.open.tab, { noremap = true, silent = true })
-            end
         })
 
         -- changes nvim-tree to transparent background
@@ -58,7 +23,7 @@ return {
             highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
         ]]
 
-        -- for oepning and closing the nvim tree
+        -- for opening and closing the nvim tree
         vim.keymap.set('n', '<leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
         -- for moving the cursor in the tree to the current buffer
