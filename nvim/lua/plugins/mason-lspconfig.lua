0bfc1da99a2c8750142609645a76ecaf71419d5c
angkiki <gabrielangzw@gmail.com>
2025-05-07 10:13:35 +0800

    version lock mason-lspconfig

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,11 +1,12 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "ts_ls" } -- Auto-install these
-    })
-  end
+    "williamboman/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            automatic_enabled = false,
+            ensure_installed = { "lua_ls", "ts_ls" } -- Auto-install these
+        })
+    end
 }
-
