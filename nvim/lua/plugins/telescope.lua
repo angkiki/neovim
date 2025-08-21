1eca79b3db53931981116c662b860006db98e521
gabrielang-fp <gabriel.ang@foodpanda.com>
2025-08-21 16:38:18 +0800

    update 0.11 configs

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,9 +11,11 @@ return {
             defaults = { winblend = 0, border = true },
         })
 
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
-        vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find git files" })
-        vim.keymap.set("n", "<leader>pb", ":Telescope buffers<CR>", { desc = "Telescope show buffer files" })
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "telescope: open raw file finder" })
+        vim.keymap.set("n", "<C-p>", builtin.git_files,
+            { desc = "telescope: open file finder within current git repository" })
+        vim.keymap.set("n", "<leader>pb", ":Telescope buffers<CR>",
+            { desc = "telescope: display files in current buffer " })
 
         -- ripgrep is needed to be installed for this to work
         -- to install in WSL:
@@ -25,12 +27,11 @@ return {
             if search and search ~= "" then
                 builtin.grep_string({ search = search, cwd = vim.fn.getcwd(), fixed_strings = true })
             end
-        end)
+        end, { desc = "telescope: ripgrep string search, results displayed in telescope" })
 
         vim.keymap.set("n", "<leader>psl", function()
             -- live search using ripgrep's regex engine
             builtin.live_grep()
-        end)
+        end, { desc = "telescope: open live file search with fuzzy finder" })
     end
 }
-
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,26 +1,26 @@
 return {
-	"akinsho/toggleterm.nvim",
-	config = function()
-		local tt = require("toggleterm")
-		tt.setup({
-			direction = "float",
-		})
+    "akinsho/toggleterm.nvim",
+    config = function()
+        local tt = require("toggleterm")
+        tt.setup({
+            direction = "float",
+        })
 
-		local term = require("toggleterm.terminal").Terminal
-		local float_term = term:new({ direction = "float" })
+        local term = require("toggleterm.terminal").Terminal
+        local float_term = term:new({ direction = "float" })
 
-		-- Helper function to exit terminal insert mode
-		local function exit_terminal_mode()
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
-		end
+        -- Helper function to exit terminal insert mode
+        local function exit_terminal_mode()
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+        end
 
-		vim.keymap.set("n", "<leader>tt", function()
-			float_term:toggle()
-		end, { desc = "Toggle floating terminal" })
+        vim.keymap.set("n", "<leader>tt", function()
+            float_term:toggle()
+        end, { desc = "toggleterm: toggle display of floating terminal" })
 
-		vim.keymap.set("t", "<leader>tt", function()
-			exit_terminal_mode()
-			float_term:toggle()
-		end, { desc = "Toggle floating terminal (terminal mode)" })
-	end,
+        vim.keymap.set("t", "<leader>tt", function()
+            exit_terminal_mode()
+            float_term:toggle()
+        end, { desc = "toggleterm: toggle display of floating terminal (terminal mode)" })
+    end,
 }
