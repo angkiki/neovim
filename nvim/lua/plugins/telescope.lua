14227486154740010609480bf120566369f67696
gabrielang-fp <gabriel.ang@foodpanda.com>
2026-02-04 16:20:12 +0800

    update telescope function to support multiple file opening

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,10 +5,40 @@ return {
     config = function()
         local telescope = require("telescope")
         local builtin = require("telescope.builtin")
+        local actions = require("telescope.actions")
+        local action_state = require("telescope.actions.state")
+
+        -- Custom action to open all selected files, or current file if none selected
+        local select_one_or_multi = function(prompt_bufnr)
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local multi = picker:get_multi_selection()
+            if not vim.tbl_isempty(multi) then
+                actions.close(prompt_bufnr)
+                for _, j in pairs(multi) do
+                    if j.path ~= nil then
+                        vim.cmd(string.format("%s %s", "edit", j.path))
+                    end
+                end
+            else
+                actions.select_default(prompt_bufnr)
+            end
+        end
 
         telescope.setup({
             -- Your Telescope config here
-            defaults = { winblend = 0, border = true },
+            defaults = {
+                winblend = 0,
+                border = true,
+                path_display = { "truncate" },
+                mappings = {
+                    i = {
+                        ["<CR>"] = select_one_or_multi,
+                    },
+                    n = {
+                        ["<CR>"] = select_one_or_multi,
+                    },
+                },
+            },
         })
 
         vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "telescope: open raw file finder" })
