8a58a34e8737b841950a75bc39dedb0eb0af44ba
gabriel-tcs <g.ang@thecloudscore.com>
2026-02-16 11:15:16 +0700

    add json formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
         conform.setup({
             formatters_by_ft = {
                 lua = { "stylua" },
+                json = { "prettierd", "prettier", stop_after_first = true },
                 javascript = { "prettierd", "prettier", stop_after_first = true },
                 typescript = { "prettierd", "prettier", stop_after_first = true },
                 typescriptreact = { "prettierd", "prettier", stop_after_first = true },
